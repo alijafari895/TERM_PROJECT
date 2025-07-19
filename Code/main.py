d839def83c50bf0e2f181dacacbb7ece109ff28f
alijafari895 <alijafari895@gmail.com>
2025-07-19 23:36:34 +0330

    add comnts

--- Code/main.py
+++ Code/main.py
@@ -1,14 +1,18 @@
-from fastapi import FASTAPI , HTTPExeption , Depends
+#add library
+from fastapi import FastAPI , HTTPException , Depends
 from sqlalchemy.orm import Session
 from pydantic import BaseModel , EmailStr
 
 from model import Supplier , Base
-from database import engine , SessionLocal
+from databse import engine , SessionLocal
 
+#make engine
 Base.metadaa.creat_all(bind=engine)
 
+#make app
 app = FastAPI()
 
+#make db func and sesion maker
 def get_db():
     db = SessionLocal()
     try:
@@ -16,15 +20,19 @@ def get_db():
     finally:
         db.close()
 
+#make acceptable value for supplier
 class SupplierCreat(BaseModel):
     name: str
     email: EmailStr
     contact: str | None = None
     delivery_days = int
 
+#make acceptable value for active or not
 class Supplierresponse(SupplierCreat):
     id: int
     is_active: bool
 
     class Config:
         orm_mode = True
+
+
