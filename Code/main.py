4c00787ea158adeb78d7353fdf372969b8379f90
alijafari895 <alijafari895@gmail.com>
2025-07-19 23:05:27 +0330

    add connection and db function for session

--- Code/main.py
+++ Code/main.py
@@ -3,4 +3,15 @@ from sqlalchemy.orm import Session
 from pydantic import BaseModel , EmailStr
 
 from model import Supplier , Base
-from database import engine , Sessionlocal
+from database import engine , SessionLocal
+
+Base.metadaa.creat_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
