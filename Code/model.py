68cc81811c91ee3f9e33c8b8a5a25252e63fe5d1
alijafari895 <alijafari895@gmail.com>
2025-07-19 22:55:35 +0330

    add models and table for suppliers

---
+++ Code/model.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column , Integer , String , Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Supplier(Base):
+    __tablename__ = "suppliers"
+
+    id = Column ( Integer , primary_key=True , index=True )
+    name = Column ( String , nullable=False )
+    email = Column ( String , unique=True , nullable=False ) 
+    Phone = Column ( Integer , unique=True , nullable=False )
+    delivery_days = Column ( Integer )
+    is_active = Column ( Boolean , default=True )
