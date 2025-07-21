16a78b70b28b08b66e38d038050e84b3ee9656d0
alijafari895 <alijafari895@gmail.com>
2025-07-21 18:00:49 +0330

    clean the code

--- Code/main.py
+++ Code/main.py
@@ -1,14 +1,10 @@
 #add library
 from fastapi import FastAPI , HTTPException , Depends
 from sqlalchemy.orm import Session
-from pydantic import BaseModel , EmailStr
-from pydantic import Field
-from model import Supplier , Base
 from databse import engine , SessionLocal
-from typing import Optional
-from schema import SupplierCreate , SupplierResponse , SupplierUpdate
 from databse import get_db  
 from Routes import Supplier
+from model import Supplier , Base
 
 #make engine
 Base.metadata.create_all(bind=engine)
