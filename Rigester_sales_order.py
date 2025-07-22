1860a7c461d24e46f4354c25bb0ad9732565ae29
Saleh Estiri <197503599+SalehEstiri@users.noreply.github.com>
2025-07-22 18:46:16 +0330

    An HTTP exception added.

--- Rigester_sales_order.py
+++ Rigester_sales_order.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 import sqlite3
 
 router = APIRouter()
@@ -7,5 +7,20 @@ def check_inventory(number, inventory):
     return True if number <= inventory else False
 
 @router.post('/Dashboard/Register_orders', tags=['Register_orders'])
-def register_orders(name: str, number: int):
+def register_orders(name: str, number: int):    
+    with sqlite3.connect('Product_info.db') as conn:
+        cur = conn.cursor()
+        cur.execute('Select Inventory FROM product WHERE name=?', (name,))
+        inventory = cur.fetchone()
+        
+        if inventory:
+            if check_inventory(number, inventory):
+                
+            else:
+                raise HTTPException(status_code=409, 
+                                    detail="""Sorry, we don't have enough stock for your order.
+                                    Please try a smaller quantity or check back later!""")
+        else:
+            return{"Result" : "Product not founded !"}
+    
     return
