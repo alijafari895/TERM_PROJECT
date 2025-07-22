408d58d008b323ee69652ab6b1f7af959e8a5b2e
Saleh Estiri <197503599+SalehEstiri@users.noreply.github.com>
2025-07-22 18:36:07 +0330

    Check_inventory function added.

--- Rigester_sales_order.py
+++ Rigester_sales_order.py
@@ -3,6 +3,9 @@ import sqlite3
 
 router = APIRouter()
 
+def check_inventory(number, inventory):
+    return True if number <= inventory else False
+
 @router.post('/Dashboard/Register_orders', tags=['Register_orders'])
-def register_orders():
+def register_orders(name: str, number: int):
     return
