32ee677634b8d4b21682ba64c4e239df7e555dbd
Saleh Estiri <197503599+SalehEstiri@users.noreply.github.com>
2025-07-22 19:17:32 +0330

    Update product inventory added.

--- Rigester_sales_order.py
+++ Rigester_sales_order.py
@@ -4,10 +4,10 @@ import sqlite3
 router = APIRouter()
 
 def check_inventory(number, inventory):
-    return True if number <= inventory else False
+    return True if number <= inventory[0] else False
 
 @router.post('/Dashboard/Register_orders', tags=['Register_orders'])
-def register_orders(name: str, number: int):    
+def register_orders(name: str, number: int=1):    
     with sqlite3.connect('Product_info.db') as conn:
         cur = conn.cursor()
         cur.execute('Select Inventory FROM product WHERE name=?', (name,))
@@ -15,12 +15,12 @@ def register_orders(name: str, number: int):
         
         if inventory:
             if check_inventory(number, inventory):
-                
+               cur.execute('UPDATE product SET Inventory = Inventory - ? WHERE name=?', (number, name)) 
+               conn.commit()
+               return{"Result": "Ordered registered seccessfully!"}
             else:
                 raise HTTPException(status_code=409, 
-                                    detail="""Sorry, we don't have enough stock for your order.
-                                    Please try a smaller quantity or check back later!""")
+                                    detail="""Sorry, we don't have enough stock for your order. Please try a smaller quantity or check back later!""")
         else:
-            return{"Result" : "Product not founded !"}
-    
-    return
+            raise HTTPException(status_code=404, detail='Product not founded!')
+        
